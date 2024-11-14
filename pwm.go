4fe19c2abf28dc8e71b9d53dd375b16b22404ec2
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2024-11-14 12:26:41 +0100

    Renormalized line endings to LF

--- pwm.go
+++ pwm.go
@@ -3,25 +3,17 @@ package pwm
 import (
 	"net/http"
 
-	"github.com/nixpare/server/v2"
+	"github.com/nixpare/nix"
+	"github.com/nixpare/server/v3"
 )
 
-var (
-	PWM = server.Website {
-		Name: "PWM",
-		Dir: basedir,
-		MainPages: []string{"/"},
-		NoLogPages: []string{"/assets/"},
-	}
-)
-
-func PWMInit(srv *server.HTTPServer, domain *server.Domain, subdomain *server.Subdomain) error {
-	err := srv.Router.TaskManager.NewProcess(WORDLE_PROGRAM_NAME, basedir, "node", ".")
+func PWMInit(router *server.Router) error {
+	_, err := router.TaskManager.NewProcess(WORDLE_PROGRAM_NAME, basedir, "node", ".")
 	if err != nil {
 		return err
 	}
 
-	err = srv.Router.TaskManager.StartProcess(WORDLE_PROGRAM_NAME)
+	err = router.TaskManager.StartProcess(WORDLE_PROGRAM_NAME)
 	if err != nil {
 		return err
 	}
@@ -29,9 +21,11 @@ func PWMInit(srv *server.HTTPServer, domain *server.Domain, subdomain *server.Su
 	return nil
 }
 
-func PWMRoute(route *server.Route) {
-	err := route.ReverseProxy("http://127.0.0.1:8000")
-	if err != nil {
-		route.Error(http.StatusBadGateway, "Bad Gateway", err)
-	}
+func PWM() http.Handler {
+	return nix.NewHandler(func(ctx *nix.Context) {
+		err := ctx.ReverseProxy(proxy_addr)
+		if err != nil {
+			ctx.Error(http.StatusBadGateway, "Bad Gateway", err)
+		}
+	}, nix.ConnectToMainOption(), nix.EnableRecoveryOption())
 }
